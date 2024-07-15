76b92111a9f97deebe18b9fddb756db2bbc44664
Ine Lofthus Arnesen <inelofthus@gmail.com>
2024-07-15 13:09:29 +0200

    Legg til transitions, litt mer styling

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,7 +1,20 @@
-document
-    .querySelector(".hamburger")
-    .addEventListener("click", () => {
-        document
-            .querySelector(".nav-links")
-            .classList.toggle("expanded");
-    });
+document.addEventListener('astro:page-load', () => {
+    const mql = window.matchMedia("(max-width: 636px)")
+    mql.addEventListener("change", closeMenu)
+
+    document
+        .querySelector(".hamburger")
+        .addEventListener("click", toggleMenu);
+})
+
+function closeMenu() {
+    document
+        .querySelector(".nav-links")
+        .classList.remove("expanded");
+}
+
+function toggleMenu() {
+    document
+        .querySelector(".nav-links")
+        .classList.toggle("expanded");
+}
